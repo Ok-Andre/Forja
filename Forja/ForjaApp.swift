4959bcbf78b16a44a707d6eafe378652c71dd83f
Ok-Andre <garcia.carballo.marco@gmail.com>
2026-08-10 10:56:27 -0600

    Initial Commit

---
+++ Forja/ForjaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForjaApp.swift
+//  Forja
+//
+//  Created by André on 10/08/26.
+//
+
+import SwiftUI
+
+@main
+struct ForjaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ForjaTests/ForjaTests.swift
@@ -0,0 +1,17 @@
+//
+//  ForjaTests.swift
+//  ForjaTests
+//
+//  Created by André on 10/08/26.
+//
+
+import Testing
+@testable import Forja
+
+struct ForjaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
